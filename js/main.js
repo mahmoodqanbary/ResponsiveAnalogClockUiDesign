f982d0dc4669996270e75424091fa04d9d9a2481
Mahmood Qanbary <MahmoodQanbary@gmail.com>
2021-12-23 16:39:59 +0330

    Create analog clock

---
+++ js/main.js
@@ -0,0 +1,23 @@
+/*==================== CLOCK ====================*/
+const hour = document.getElementById('clock-hour'),
+    minutes = document.getElementById('clock-minutes'),
+    seconds = document.getElementById('clock-seconds');
+
+const clock = () => {
+    let date = new Date();
+
+    let hh = date.getHours() * 30,
+        mm = date.getMinutes() * 6,
+        ss = date.getSeconds() * 6;
+
+    //We add rotation to the elements
+    hour.style.transform = `rotateZ(${hh + mm / 12}deg)`;
+    minutes.style.transform = `rotateZ(${mm}deg)`;
+    seconds.style.transform = `rotateZ(${ss}deg)`;
+}
+setInterval(clock, 1000) //1000 = 1s
+
+/*==================== CLOCK & DATE TEXT ====================*/
+
+/*==================== DARK/LIGHT THEME ====================*/
+
